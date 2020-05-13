9b0021e3c8cdc57f506139c5a3f50bc0ebad18f6
Igor <i.volobouev@ttu.edu>
2020-05-13 18:29:52 +0200

    dumping ADCs as unsigned

--- RecoLocalCalo/HcalRecProducers/src/HBHEPhase1Reconstructor.cc
+++ RecoLocalCalo/HcalRecProducers/src/HBHEPhase1Reconstructor.cc
@@ -262,6 +262,7 @@ private:
 
   // Configuration parameters
   std::string dumpFile_;
+  unsigned dumpMax_;
   std::string algoConfigClass_;
   bool processQIE8_;
   bool processQIE11_;
@@ -336,6 +337,7 @@ private:
 //
 HBHEPhase1Reconstructor::HBHEPhase1Reconstructor(const edm::ParameterSet& conf)
     : dumpFile_(conf.getUntrackedParameter<std::string>("dumpFile", "")),
+      dumpMax_(conf.getUntrackedParameter<unsigned>("dumpMax", 10U)),
       algoConfigClass_(conf.getParameter<std::string>("algoConfigClass")),
       processQIE8_(conf.getParameter<bool>("processQIE8")),
       processQIE11_(conf.getParameter<bool>("processQIE11")),
@@ -426,7 +428,7 @@ void HBHEPhase1Reconstructor::processData(const Collection& coll,
   // Note that this flag affects only "infos", the rechits are still
   // not going to be constructed from such channels.
   const bool skipDroppedChannels = !(infos && saveDroppedInfos_);
-  const bool dumpInfos = dump_.is_open();
+  const bool dumpInfos = dump_.is_open() && counter_ < dumpMax_;
 
   // Iterate over the input collection
   for (typename Collection::const_iterator it = coll.begin(); it != coll.end(); ++it) {
@@ -752,6 +754,7 @@ void HBHEPhase1Reconstructor::fillDescriptions(edm::ConfigurationDescriptions& d
   desc.add<edm::InputTag>("digiLabelQIE8");
   desc.add<edm::InputTag>("digiLabelQIE11");
   desc.addUntracked<std::string>("dumpFile");
+  desc.addUntracked<unsigned>("dumpMax");
   desc.add<std::string>("algoConfigClass");
   desc.add<bool>("processQIE8");
   desc.add<bool>("processQIE11");
